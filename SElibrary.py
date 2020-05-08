94e866e4ddccd8ccb255bbb3d906f12954f3245f
kousukeex1997 <kousukeex1997@gmail.com>
2020-05-08 14:46:56 +0900

    upload
    Upload files.

---
+++ SElibrary.py
@@ -0,0 +1,7 @@
+import os
+
+# サウンドエフェクトライブラリ
+# PBomb = os.path.join(os.getcwd(),"se\\explosion2.wav")
+# shoot = os.path.join(os.getcwd(),"se\\short_bomb.wav")
+# Player_Hit = os.path.join(os.getcwd(),"se\\Player_hit.wav")
+# item_get = os.path.join(os.getcwd(),"se\\item_get.wav")
